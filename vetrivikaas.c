fee094437e44eb0bf47837df6e4f13fcc11327f5
ABIAMMU <31499317+ABIAMMU@users.noreply.github.com>
2018-01-31 15:51:40 +0530

    vetrivikaas.c

---
+++ vetrivikaas.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	char words[100];
+	int spaces=0,i;
+	
+	printf("enter a line of text:\n");
+	if(words[i]=='^~')
+	{
+		spaces++;
+	
+	}
+	printf("\n number of white spaces :%d",spaces);
+	
+
+}
+	
