4fbea3dd28f100ec1a3ca8b6ed17537869fff1db
ABIAMMU <31499317+ABIAMMU@users.noreply.github.com>
2018-02-09 15:40:55 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a=5,b=3,sum;
+    printf("enter the two numbers");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("the sum of two numbers",sum);
+    return 0;
+}
