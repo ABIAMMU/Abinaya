f410ff0969e4968ddba0fdcae66026febaa04bb9
ABIAMMU <31499317+ABIAMMU@users.noreply.github.com>
2018-02-11 12:56:44 +0800

    without arg & without ret.c

---
+++ 123/without arg & without ret.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+void sub();
+void main()
+{
+   
+    sub();
+
+}
+
+void sub()
+{
+    int x,y,z;
+    x=10,y=5;
+    z=x-y;
+    printf("the subtracted value is %d",z);
+}
