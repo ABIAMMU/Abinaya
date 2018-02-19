088b554ba9800aacd8868b90d8e7cb161c16d456
ABIAMMU <31499317+ABIAMMU@users.noreply.github.com>
2018-02-19 15:53:40 +0530

    Create copy.c

---
+++ copy.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int n,d,rem=0;
+ printf("enter the number");
+ scanf("%d",&n);
+ while(n!=0)
+ {
+   d=n%10;
+   n=n/10;
+   rem=rem+digit;
+   }
+   printf("the sum of digits %d",rem);
+   return 0;
+   }
