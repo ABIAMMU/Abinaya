ad199ef10c25877c9ff627fee08831ec16ed0efe
ABIAMMU <31499317+ABIAMMU@users.noreply.github.com>
2018-02-26 16:46:37 +0530

    Create addoddeven.c

---
+++ addoddeven.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int i,j,k,d;
+	printf("\nEnter the two numbers: ");
+	scanf("%d",&i);
+	scanf("%d",&j);
+		k=i+j;
+	if(d%2==0)
+	{
+		printf("\nThe sum is even.");
+	}
+	else
+	{
+		printf("\nThe sum is odd. ");
+	}
+	return 0;
+}
