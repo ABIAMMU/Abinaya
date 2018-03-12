9ee24d26d751388db0397cecb019399cf6b74457
ABIAMMU <31499317+ABIAMMU@users.noreply.github.com>
2018-03-12 15:43:47 +0530

    Create nearest.c

---
+++ nearest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	float n;
+	int x;
+  clrscr();
+	printf("\n enter the num:");
+	scanf("%f",&n);
+	x=(int)(n+0.5);
+	printf("\n%d",x);
+  getch();
+	return 0;
+}
